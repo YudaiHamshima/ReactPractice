2855946b73464b41d0d1e65c13ec9adaa30f4d33
YudaiHamashima <yudai.hamashima@enrise-corp.co.jp>
2022-06-22 18:07:51 +0900

    ready to table making

---
+++ react-practice/src/crud/allPayments.tsx
@@ -0,0 +1,10 @@
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { categoryState, paymentState } from '../recoil/atoms'
+
+function AllPayments() {
+    return(
+        <h1>Payments</h1>
+    )
+}
+
+export default AllPayments
---
+++ react-practice/src/crud/createPayment.tsx
@@ -0,0 +1,10 @@
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { categoryState, paymentState } from '../recoil/atoms'
+
+function CreatePayment() {
+    return(
+        <h1>Create</h1>
+    )
+}
+
+export default CreatePayment
